a2c84cfd845d7d55b89668d501b8a6f7967c3dab
Luca Bettoni Design <lucabettonidesign@gmail.com>
2024-05-24 12:27:36 +0200

    add the lenaguage manager file to remove the full script from webflow pages

---
+++ language-manager.js
@@ -0,0 +1,48 @@
+import i18next from 'https://deno.land/x/i18next/index.js';
+window.i18next = i18next; // Salva i18next nell'oggetto globale window
+
+async function loadJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok' + response.statusText);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+let _enJsonUrl = "https://raw.githubusercontent.com/luca-bettoni-pinpoint/pinpoint-website-translations/main/pinpoint-web-en.json";
+let _itJsonUrl = "https://raw.githubusercontent.com/luca-bettoni-pinpoint/pinpoint-website-translations/main/pinpoint-web-it.json";
+let _enData = await loadJSON(_enJsonUrl);
+let _itData = await loadJSON(_itJsonUrl);
+
+function updateContent() {
+    document.querySelectorAll('[data-i18n]').forEach(function (element) {
+        element.innerHTML = i18next.t(element.getAttribute('data-i18n'));
+    });
+}
+
+window.i18next.init({
+    lng: 'it', // se stai usando un rilevatore di lingua, non definire l'opzione lng
+    debug: true,
+    resources: {
+        en: {
+            translation: _enData
+        },
+        it: {
+            translation: _itData
+        }
+    }
+}).then(function (t) {
+    updateContent();
+    document.getElementById('btnEn').addEventListener('click', () => {
+        i18next.changeLanguage('en', updateContent);
+    });
+
+    document.getElementById('btnIt').addEventListener('click', () => {
+        i18next.changeLanguage('it', updateContent);
+    });
+});
