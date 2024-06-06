5a2b9df34a45253081f8b3ac6cdb2415721bab08
luca-bettoni-pinpoint <luca.bettoni@pinpointweb.it>
2024-06-06 10:58:09 +0200

    [test 03] Update main-loader.js with double quotes

--- main-loader.js
+++ main-loader.js
@@ -1,20 +1,20 @@
 // Function to dynamically load a CSS file
 function loadCSS(url) {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
     link.href = url;
     document.head.appendChild(link);
 }
 
 // Function to dynamically load a JS file
 function loadJS(url) {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = url;
     script.async = false; // to maintain execution order
     document.head.appendChild(script);
 }
 
-loadCSS('cross-style.css');
-loadJS('language-manager.js');
+loadCSS("cross-style.css");
+loadJS("language-manager.js");
 
 
